d6230bf582341e3d861b62477c92c8cb0b37aa71
Wong <allison.wong@fiserv.com>
2023-11-01 15:46:10 -0400

    AH6 Submission

--- Solution.java
+++ Solution.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.transform.Source;
@@ -14,11 +15,20 @@ class Node<T> {
 
 class Solution {
   public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+    List<String> retList = new ArrayList<>();
+    Node<String>current_node = head;
+    while (current_node != null) {
+      retList.add(current_node.val);
+      current_node = current_node.next;
+      
+    }
+    System.out.println(retList.toString());
+
+
+    return retList;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -31,5 +41,26 @@ class Solution {
 
     Solution.linkedListValues(a); 
     // -> [ "a", "b", "c", "d" ]
+
+    Node<String> hi = new Node<>("Allison");
+    Solution.linkedListValues(hi);
+
+    Node<String> hello = new Node<>("Raj");
+    Node<String> hey = new Node<>("Marcus");
+    Node<String> welcome = new Node<>("Lean");
+    Node<String> greetings = new Node<>("Henry");
+    Node<String> aloha = new Node<>("Zhou");
+    Node<String> HIII = new Node<>("Will");
+    Node<String> huh = new Node<>("Ryan");
+    huh.next = hello;
+    hello.next = hey;
+    hey.next = welcome;
+    welcome.next = hi;
+    hi.next = greetings;
+    greetings.next = aloha;
+    aloha.next = HIII;
+
+    Solution.linkedListValues(hello);
+    Solution.linkedListValues(huh);
   }
 }
